2d7f8158762c9505e831ce7e23c95285bbdb543e
Olle Delborg <odelborg@gmail.com>
2020-02-03 15:40:22 +0100

    tasks

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -15,10 +15,24 @@ let burger3 = new menuItem('Cheese', '995', 'Lactose', 'https://www.sbs.com.au/f
 let burger4 = new menuItem('Truffle', '800', 'Gluten', 'https://img.hellofresh.com/f_auto,fl_lossy,q_auto,w_1200/hellofresh_s3/image/5e0fb56c381eb932f756ea17-883cc3c6.jpg');
 let burger5 = new menuItem('BBQ', '800', 'Lactose', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmt8Fx9qUAA0h9wEro70Z0_1GqGxj4pzXz3DegVn04HhCTSG54xA&s');
 
+
+/*
+const vm = new Vue({
+  el: '#myID',
+  data: {
+    arbitraryVariableName: 'Välj en burgare'
+  }
+})
+*/
+document.getElementById("burger1").innerHTML = burger1.getBurger();
+document.getElementById("burger2").innerHTML = burger2.getBurger();
+document.getElementById("burger3").innerHTML = burger3.getBurger();
+document.getElementById("burger4").innerHTML = burger4.getBurger();
+document.getElementById("burger5").innerHTML = burger5.getBurger();
+
+
 console.log(burger1.getBurger());
 console.log(burger2.getBurger());
 console.log(burger3.getBurger());
 console.log(burger4.getBurger());
 console.log(burger5.getBurger());
-
-document.getElementById("myID").innerHTML = "Select a Burger";
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,13 @@
-const vm = new Vue({
-  el: '#myID',
-  data: {
-    arbitraryVariableName: 'Select a Burger'
-  }
-})
+window.onload = function () {
+    const vm = new Vue({
+	el: '#myID',
+	data: {
+	    IDitem: 'Välj en burgare'
+	    burger1: burger1.getBurger(),
+	    burger2: burger2.getBurger(),
+	    burger3: burger3.getBurger(),
+	    burger4: burger4.getBurger(),
+	    burger5: burger5.getBurger()
+	}
+    });
+}
