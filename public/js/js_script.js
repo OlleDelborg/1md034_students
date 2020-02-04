9d3276b6002046f882fd880fab75eab9654f964f
Olle Delborg <odelborg@gmail.com>
2020-02-04 15:40:40 +0100

    tasks 8 really

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -8,13 +8,13 @@ function menuItem(name, kcal, allergies, src) {
 	return this.name + ' ' + this.kcal;
     }
 }
-/*
+
 let burger1 = new menuItem('Fire', '773', 'Gluten', 'https://images6.alphacoders.com/883/883558.jpg');
 let burger2 = new menuItem('Turkey', '729', 'Gluten', 'https://img1.southernliving.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2017/07/main/ranch_turkey_burgers_2525701_hiaha_0086.jpg?itok=sfDFAJMw');
 let burger3 = new menuItem('Cheese', '995', 'Lactose', 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/lotus-burger-lead.jpg');
 let burger4 = new menuItem('Truffle', '800', 'Gluten', 'https://img.hellofresh.com/f_auto,fl_lossy,q_auto,w_1200/hellofresh_s3/image/5e0fb56c381eb932f756ea17-883cc3c6.jpg');
 let burger5 = new menuItem('BBQ', '800', 'Lactose', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmt8Fx9qUAA0h9wEro70Z0_1GqGxj4pzXz3DegVn04HhCTSG54xA&s');
-*/
+
 /*
 console.log(burger1.getBurger());
 console.log(burger2.getBurger());
@@ -31,7 +31,7 @@ const vm = new Vue({x
 })
 */
 
-
+/*
 let firstName = document.createElement("h2");
 firstName.innerHTML = "Fire Burger";
 document.getElementById("fstBurger").appendChild(firstName);
@@ -117,5 +117,6 @@ document.getElementById("burger2").innerHTML = burger2.getBurger();
 document.getElementById("burger3").innerHTML = burger3.getBurger();
 document.getElementById("burger4").innerHTML = burger4.getBurger();
 document.getElementById("burger5").innerHTML = burger5.getBurger();
-*/
 
+
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,15 +1,28 @@
-/*
 window.onload = function () {
     const vm = new Vue({
-	el: '#IDitem',
+	el: '#fstBurger',
 	data: {
-	    IDitem: 'Välj en burgare'
-	    burger1: burger1.getBurger(),
-	    burger2: burger2.getBurger(),
-	    burger3: burger3.getBurger(),
-	    burger4: burger4.getBurger(),
-	    burger5: burger5.getBurger()
+	    Name1: "FIRE",
+	    picture1: "https://images6.alphacoders.com/883/883558.jpg",
+	    info1: ["Hot as Hell", "Totally Vegan", "SAUCY"],
+	}
+    });
+
+     const vm2 = new Vue({
+	el: '#sndBurger',
+	data: {
+	    Name2: "TURKEY",
+	    picture2: "https://images6.alphacoders.com/883/883558.jpg",
+	    info2: ["Turkish", "GlutOUT", "Healthy"]
+	}
+     });
+
+     const vm3 = new Vue({
+	el: '#trdBurger',
+	data: {
+	    Name3: "CHEESE",
+	    picture3: "https://images6.alphacoders.com/883/883558.jpg",
+	    info3: ["Shauncy", "Totally tasty", "SAUCYBAWSY"]
 	}
     });
 }
-*/
