12b89dbf3658877852cdbc0c83fd2ad861f44070
Olle Delborg <odelborg@gmail.com>
2020-02-11 13:17:23 +0100

    410

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,28 +1,85 @@
+
 window.onload = function () {
-    const vm = new Vue({
+    const vm1 = new Vue({
 	el: '#fstBurger',
 	data: {
-	    Name1: "FIRE",
-	    picture1: "https://images6.alphacoders.com/883/883558.jpg",
-	    info1: ["Hot as Hell", "Totally Vegan", "SAUCY"],
+	    Name1: burgers[0].name,
+	    picture1: burgers[0].img,
+	    info1: [burgers[0].gluten, burgers[0].lactose, burgers[0].kCal]
 	}
     });
 
-     const vm2 = new Vue({
+    const vm2 = new Vue({
 	el: '#sndBurger',
 	data: {
-	    Name2: "TURKEY",
-	    picture2: "https://images6.alphacoders.com/883/883558.jpg",
-	    info2: ["Turkish", "GlutOUT", "Healthy"]
+	    Name2: burgers[1].name,
+	    picture2: burgers[1].img,
+	    info2: [burgers[1].gluten, burgers[1].lactose, burgers[1].kCal]
 	}
-     });
+    });
 
-     const vm3 = new Vue({
+    const vm3 = new Vue({
 	el: '#trdBurger',
 	data: {
-	    Name3: "CHEESE",
-	    picture3: "https://images6.alphacoders.com/883/883558.jpg",
-	    info3: ["Shauncy", "Totally tasty", "SAUCYBAWSY"]
+	    Name3: burgers[2].name,
+	    picture3: burgers[2].img,
+	    info3: [burgers[2].gluten, burgers[2].lactose, burgers[2].kCal]
+	}
+    });
+
+    const genders = new Vue({
+	el: '#Genders',
+	data: {
+	    gender: '',
+	    
+	},
+    });
+
+    
+    const order = new Vue({
+	el: '#orders',
+	data: {
+            
+	    name: '',
+	    email: '',
+	    address: '',
+	    house: '',
+	    pay: '',
+	    Gender: '',
+	    checkbox1: "",
+	    checkbox2: "",
+	    checkbox3: ""
+	},
+
+	methods: {
+	    markDone: function() {
+		order.name = document.getElementById("name").value;
+		order.email = document.getElementById("email").value;
+		order.address = document.getElementById("address").value;
+		order.house = document.getElementById("house").value;
+		order.pay = document.getElementById("pay").value;
+		order.Gender = genders.gender;
+
+		if(document.getElementById("checkbox1").checked) {
+		    order.checkbox1 = "burger1";
+		}
+		
+		else {order.checkbox1 = "";}
+		
+		if(document.getElementById("checkbox2").checked) {
+		    order.checkbox2 = "burger2";
+		}
+		
+		else {order.checkbox2 = "";}
+		
+		if(document.getElementById("checkbox3").checked) {
+		    order.checkbox3 = "burger3";
+		}
+		
+		else {order.checkbox3 = "";}
+		
+	    }
 	}
     });
 }
+
